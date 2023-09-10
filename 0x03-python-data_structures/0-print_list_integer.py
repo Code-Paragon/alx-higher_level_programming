6d9c0c80b8a871bedf54e48974463902ddef318c
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2023-09-10 09:18:19 -0700

    Commented

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """print integer in list
+
+    Args:
+        my_list: the list to print from
+
+    Returns:
+        NULL
+    """
+
     for i in my_list:
-        print(i)
+        print("{:d}".format(i))
