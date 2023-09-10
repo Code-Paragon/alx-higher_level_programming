f2b0ad664d0c4cf16b2e652a8f2792e7bc69a06f
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2023-09-10 13:32:17 -0700

    fixed spaces and newline error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -11,6 +11,6 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None or len(matrix) == 0:
         return
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=' ')
-        print()
+    # Convert integers to strings and join with spaces
+        row_str = ' '.join(map(str, row))
+        print("{}".format(row_str))
