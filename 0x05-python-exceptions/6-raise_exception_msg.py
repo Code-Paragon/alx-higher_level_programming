52c14b2111004e300fca8a1d994948e15a7fb730
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2023-09-25 16:22:02 -0700

    function that raises a name exception with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """raises a name exception with a message.
+
+    Args:
+        void
+
+    Returns:
+        void
+    """
+    raise NameError(message)
