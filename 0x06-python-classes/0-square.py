81ef4445592c61240e1d568e3c0a3a69a20c3425
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2023-09-30 07:56:18 -0700

    added documentaion

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+""" a module that declares a class square"""
 
 class Square:
-    """an empty class Square that defines a square
-
-    Attributes:
-        void
-    """
+    """an empty class Square that defines a square"""
     pass
