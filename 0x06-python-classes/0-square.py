dd8bc68c4644906ee2601cca9e4b20da52927b60
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2023-09-30 08:54:02 -0700

    a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-""" a module that declares a class square"""
+
+""" a module that declares a class square
+
+"""
+
 
 class Square:
-    """an empty class Square that defines a square"""
+    """an empty class Square that defines a square
+
+    """
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a module that defines a square by: (based on 0-square.py)
+
+"""
+
+
+class Square:
+    """ a class defining a square
+
+    """
+    def __init__(self, size):
+        """initializes the size of the square
+
+        Args:
+            size: the squares size
+        """
+        self.__size = size
