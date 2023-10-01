8fa8ad08e19b194e14b4277d26db62914b5025aa
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2023-10-01 03:07:14 -0700

    pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+""" this module defines a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """this class Represents a square."""
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
